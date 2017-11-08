49dd33d70a0641f19162221a5ed21081aa94ab06
Will Osborne <w.osborne@warwick.ac.uk>
2017-11-08 16:31:28 +0000

    blah

--- lab3/thread.c
+++ lab3/thread.c
@@ -1,24 +1,35 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+
+static pthread_mutex_t muxlock = PTHREAD_MUTEX_INITIALIZER;
 
 void *thread_code (void *arg) {
-  printf("threads LMFAO id: %d\n", pthread_getthreadid_np());
+  pthread_mutex_lock(&muxlock);
+  printf("Adding 10000!\n");
+  int *ptr = (int *) arg;
+  int i = 0;
+  for (i = 0; i < 10000; i++)
+    *ptr = *ptr + 1;
+  pthread_mutex_unlock(&muxlock);
   pthread_exit(NULL);
 }
 
 int main () {
-
   pthread_t threads[10];
+  int total = 0;
   int i;
+
   for (i = 0; i < 10; i++) {
-    pthread_create(&threads[i], NULL, &thread_code, (void *) NULL);
+    pthread_create(&threads[i], NULL, &thread_code, &total);
   }
 
   for (i = 0; i < 10; i++) {
     pthread_join(threads[i], (void *) NULL);
   }
 
+  printf("\nTotal: %d\n", total);
 
   exit(EXIT_SUCCESS);
 }
